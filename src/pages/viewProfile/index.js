de4a9cd76f05a7a5596c79fe462bc059a0cb8cb2
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-02-06 19:30:34 +0000

    Improved the spacing on CSS and placed the profile-details content inside the Card component

--- src/pages/viewProfile/index.js
+++ src/pages/viewProfile/index.js
@@ -4,9 +4,9 @@ import Button from "../../components/button";
 import Header from "../../components/header";
 import Navigation from "../../components/navigation";
 import useAuth from "../../hooks/useAuth";
-import ProfileIcon from "../../assets/icons/profileIcon"
-import "../../pages/viewProfile/viewProfile.css"
-
+import ProfileIcon from "../../assets/icons/profileIcon";
+import "../../pages/viewProfile/viewProfile.css";
+import Card from "../../components/card";
 
 // import "./src/dashboard/style.css"
 
@@ -60,41 +60,81 @@ function ViewProfile() {
   return (
     <>
       {/* create the jsx for the all details */}
-      <div className = "viewProfile-container">
+
+      <div className="viewProfile-container">
         <Header />
         <Navigation />
-        <div className="profile-details">
-          {/* headings to add  */}
-           {/* write the user name */} 
-          <div className="profile-header">
-            <h2>
-          <ProfileIcon />
-          {profile.firstName} {profile.lastName}</h2>
-          </div>
+        <div className="card-container">
+          <Card>
+            <div className="profile-details">
+              {/* headings to add  */}
+              {/* write the user name */}
+              <div className="profile-header">
+                <h2>
+                  <ProfileIcon />
+                  {profile.firstName} {profile.lastName}
+                </h2>
+              </div>
 
-          <div className= "profile-contact">
-          <h3>Contact details</h3>
-          <h4>Email </h4>
-          <p>{profile.email}</p>
-          <h4>Mobile Number</h4>
-          <p>+123456789</p>
-          <h4>Github UserName</h4>
-           <p>{profile.githubUrl}</p>
-          </div>
+              <div className="profile-contact">
+                <h3>Contact details</h3>
 
-          <div className="profile-bio">
-          <h4>Biography</h4>
-          <p> {profile.biography}</p>
-          </div>
+                <div>
+                  <h4>Email </h4>
+                  <p>{profile.email}</p>
+                </div>
 
-          <div className="profile-training">
-          <h3>Training</h3>
-          <h4>Role</h4>
-          <p>{profile.role}</p>
+                <div>
+                  <h4>Mobile Number</h4>
+                  <p>+123456789</p>
+                </div>
 
-          </div>
-          {/* add the onclick event for the button it is going to popen the edit page */}
-          <Button text="Edit" classes="green width-full" />
+                <div>
+                  <h4>Github Username</h4>
+                  <p>{profile.githubUrl}</p>
+                </div>
+              </div>
+
+              <div className="profile-bio">
+                <h3>Biography</h3>
+                <p> {profile.biography}</p>
+              </div>
+
+              <div className="profile-training">
+                <h3>Training info</h3>
+
+                <div>
+                  <h4>Role</h4>
+                  <p>{profile.role}</p>
+                </div>
+
+                <div>
+                  <h4>Specialism</h4>
+                  <p>Software Developer</p>
+                </div>
+
+                <div>
+                  <h4>Cohort</h4>
+                  <p>Cohort 9</p>
+                </div>
+
+                <div>
+                  <h4>Start Date</h4>
+                  <p>November 2022</p>
+                </div>
+
+                <div>
+                  <h4>End Date</h4>
+                  <p>May 2023</p>
+                </div>
+              </div>
+
+              {/* add the onclick event for the button it is going to popen the edit page */}
+              <div className="edit-button">
+                <Button text="Edit" classes="green width-full" />
+              </div>
+            </div>
+          </Card>
         </div>
       </div>
     </>
