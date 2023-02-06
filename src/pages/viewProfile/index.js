52736c33452a856be6ac7218ca8ff5e6f5845422
Manoj kumar <manoj.bhoosani@gmail.com>
2023-02-06 17:47:03 +0100

    created css grid for the profile page and jsx

--- src/pages/viewProfile/index.js
+++ src/pages/viewProfile/index.js
@@ -1,6 +1,14 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import Button from "../../components/button";
+import Header from "../../components/header";
+import Navigation from "../../components/navigation";
 import useAuth from "../../hooks/useAuth";
+import ProfileIcon from "../../assets/icons/profileIcon"
+import "../../pages/viewProfile/viewProfile.css"
+
+
+// import "./src/dashboard/style.css"
 
 const initialState = {
   id: "",
@@ -33,7 +41,6 @@ function ViewProfile() {
   };
 
   useEffect(() => {
-    // if (profile)
     if (!token) {
       console.log("ERROR!!");
     } else {
@@ -43,25 +50,52 @@ function ViewProfile() {
         .then((responseData) => {
           console.log("checking Data", responseData);
           // If successfull response, add the data to the state
-          const newProfileData = responseData.data.user;
-          console.log("newProfileData: ", newProfileData);
-          const updatedProfileData = { ...profile, newProfileData };
-          // newProfileData.push();
-
-          setProfile(updatedProfileData);
           console.log("NEW DATA: ", profile);
+          setProfile(responseData.data.user);
         });
     }
   }, []);
-
-  //   console.log("THE PROFILE DATA => ", profile);
 
   // console.log("THE PROFILE DATA => ", profile);
   return (
     <>
       {/* create the jsx for the all details */}
-      <div className="">
-        <p>Something</p>
+      <div className = "viewProfile-container">
+        <Header />
+        <Navigation />
+        <div className="profile-details">
+          {/* headings to add  */}
+           {/* write the user name */} 
+          <div className="profile-header">
+            <h2>
+          <ProfileIcon />
+          {profile.firstName} {profile.lastName}</h2>
+          </div>
+
+          <div className= "profile-contact">
+          <h3>Contact details</h3>
+          <h4>Email </h4>
+          <p>{profile.email}</p>
+          <h4>Mobile Number</h4>
+          <p>+123456789</p>
+          <h4>Github UserName</h4>
+           <p>{profile.githubUrl}</p>
+          </div>
+
+          <div className="profile-bio">
+          <h4>Biography</h4>
+          <p> {profile.biography}</p>
+          </div>
+
+          <div className="profile-training">
+          <h3>Training</h3>
+          <h4>Role</h4>
+          <p>{profile.role}</p>
+
+          </div>
+          {/* add the onclick event for the button it is going to popen the edit page */}
+          <Button text="Edit" classes="green width-full" />
+        </div>
       </div>
     </>
   );
