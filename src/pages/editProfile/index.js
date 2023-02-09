1b3f8985de00ccfeb5edef1f523afc9d0db9bc9f
benji <benjipictor@gmail.com>
2023-02-09 12:57:02 +0000

    unfinished page permisions

--- src/pages/editProfile/index.js
+++ src/pages/editProfile/index.js
@@ -34,7 +34,7 @@ const EditProfile = () => {
   const [loggedInUserInfo, setLoggedInUserInfo] = useState("")
   const [isError, setIsError] = useState(false);
   const navigate = useNavigate()
-  const {id} = useParams()
+  const { id } = useParams()
   const ProfileImg = () => {
     if (profile.profileImageUrl === "") {
       return <ProfileCircle
@@ -62,12 +62,12 @@ const EditProfile = () => {
     navigate(-1)
   }
   const controlPagePermission = () => {
-    if(loggedInUserInfo.role === "STUDENT" && loggedInUserInfo.id !== profile.id){
+    if (loggedInUserInfo.role === "STUDENT" & loggedInUserInfo.id !== profile.id) {
       setIsError(true)
-      return(
-        <ErrorMessage message={"User does not have permission to edit this profile "}/>
+      return (
+        <ErrorMessage message={"User does not have permission to edit this profile "} />
       )
-    } 
+    }
   }
 
   const handleChange = (event) => {
@@ -81,188 +81,189 @@ const EditProfile = () => {
   const token = localStorage.getItem("token");
   useEffect(() => {
     getUserInfo()
-    controlPagePermission()
-    if(loggedInUserInfo.role === "STUDENT"){
+    if (loggedInUserInfo.role === "STUDENT") {
       setReadOnly(true)
       console.log("this is the read onlystatus", readOnly)
-    } 
-    if(loggedInUserInfo.id === profile.id){
+    }
+    if (loggedInUserInfo.id === profile.id) {
       setPasswordPermission(false)
-      
+
     }
     const profileData = async () => {
       const res = await get(`users/${id}`);
-     
+
       res.data.user ? setFormState(res.data.user) : setIsError(true);
       res.data.user ? setProfile(res.data.user) : setIsError(true)
       console.log("RESPONSE from GET: ", res.data.user)
-      console.log("success or fail?", res.data.status)
+      // console.log("success or fail?", res.data.status)
       console.log("this is form state", formState)
-      console.log("this is the profile", profile)
-      console.log("this is id",id)
+      console.log("this is id", id)
     };
     profileData()
-    console.log("this is the password permiossion",passwordPermission)
-    
+    console.log("this is the password permiossion", passwordPermission)
+    console.log("this is profile.id", profile.id)
+    controlPagePermission()
+
   }, [id])
+  console.log("this is the profile", profile)
 
 
-  
-return (
-  <>
-  {isError === true && <ErrorMessage message={"PROFILE NOT FOUND"} />}
-  {isError === false && (
-    <div className="editContainer">
-      <h1>Profile</h1>
-      <Card>
-        <div className="profile-header">
-          <ProfileCircle
-            initials={`${profile.firstName[0]} ${profile.lastName[0]}`}
-          />
-          <div className="profile-header-text">
-            <h4>
-              {profile.firstName} {profile.lastName}
-            </h4>
-            <p>{profile.specialism}this is the specialism</p>
-          </div>
-        </div>
 
-        <form onSubmit={showModal}>
-          <section className="basicInfoSection text-input-containers">
-            <h2>Basic Info</h2>
-            <div className="headshot-container">
-              <ProfileImg  />
-              <p>Add headshot</p>
+  return (
+    <>
+      {isError === true && <ErrorMessage message={"PROFILE NOT FOUND"} />}
+      {isError === false && (
+        <div className="editContainer">
+          <h1>Profile</h1>
+          <Card>
+            <div className="profile-header">
+              <ProfileCircle
+                initials={`${profile.firstName[0]} ${profile.lastName[0]}`}
+              />
+              <div className="profile-header-text">
+                <h4>
+                  {profile.firstName} {profile.lastName}
+                </h4>
+                <p>{profile.specialism}this is the specialism</p>
+              </div>
             </div>
-            <TextInput
-              label="First Name*"
-              name="firstName"
-              value={formState.firstName}
-              onChange={handleChange}
-            />
-            <TextInput
-              label="Last Name*"
-              name="lastName"
-              value={formState.lastName}
-              onChange={handleChange}
-            />
-            <TextInput
-              label="Username*"
-              name="userName"
-              value={formState.userName}
-              onChange={handleChange}
-            />
-            <TextInput
-              label="GitHub Username*"
-              name="githubUrl"
-              value={formState.githubUrl}
-              onChange={handleChange}
-            />
-          </section>
 
-          {profile.role === "STUDENT" ?
-            <section className="trainingInfoSection text-input-containers">
-              <h2>Training Info</h2>
-              <TextInput
-                label="Role*"
-                name="role"
-                value={formState.role.toLocaleLowerCase()}
-                onChange={handleChange}
-                readOnly={readOnly}
-              />
-              <TextInput
-                label="Specialism*"
-                name="specialism"
-                value={formState.specialism}
-                onChange={handleChange}
-                readOnly={readOnly}
-              />
-              <TextInput
-                label="Cohort*"
-                name="cohort_id"
-                value={ formState.cohort_id}
-                onChange={handleChange}
-                readOnly={readOnly}
-              />
-              <TextInput
-                label="Start Date*"
-                name="start-date"
-                value={formState.startDate}
-                onChange={handleChange}
-                readOnly={readOnly}
-              />
-              <TextInput
-                label="End Date*"
-                name="end-date"
-                value={formState.endDate}
-                onChange={handleChange}   
-                readOnly={readOnly}
-              />
-            </section>
-            :
-            <section className="proffesional-info-section">
-              <h2>Proffessional Info</h2>
-              <TextInput
-                label="Role*"
-                name="role"
-                value={profile.cohort}
-                onChange={handleChange}
-              />
-              <TextInput
-                label="Specialism*"
-                name="specialism"
-                value={formState.specialism}
-                onChange={handleChange}
-              />
-              <TextInput
-                label="Job Title*"
-                name="jobTitle"
-                value={formState.specialism}
-                onChange={handleChange}
-              />
-            </section>
-          }
-          <section className="contactInfoSection">
-            <h2>Contact Info</h2>
-            <TextInput
-              label="Email*"
-              name="email"
-              value={formState.email}
-              type="email"
-              onChange={handleChange}
-            />
-            <TextInput
-              label="Mobile*"
-              name="phone"
-              value={formState.phone}
-              onChange={handleChange}
-            />
-            <TextInput
-              label="Password*"
-              name="password"
-              value={formState.password}
-              type="password"
-              onChange={handleChange}
-              permission={passwordPermission}
-            />
-          </section>
-          <section className="bioSection">
-            <h2>Bio</h2>
-            <textarea label="Bio" name="biography" value={formState.biography} editable rows={20} maxLength="300" onChange={handleChange} />
-            <label htmlFor="bio" >{formState.biography.length}/300</label>
-          </section>
-          <section className="footer">
-            <p>Required*</p>
-            <Button text={"Cancel"} onClick={cancelChanges} classes="offwhite width-full" />
-            <Button text={"save"} type={"submit"} classes="blue width-full" />
-          </section>
-        </form>
+            <form onSubmit={showModal}>
+              <section className="basicInfoSection text-input-containers">
+                <h2>Basic Info</h2>
+                <div className="headshot-container">
+                  <ProfileImg />
+                  <p>Add headshot</p>
+                </div>
+                <TextInput
+                  label="First Name*"
+                  name="firstName"
+                  value={formState.firstName}
+                  onChange={handleChange}
+                />
+                <TextInput
+                  label="Last Name*"
+                  name="lastName"
+                  value={formState.lastName}
+                  onChange={handleChange}
+                />
+                <TextInput
+                  label="Username*"
+                  name="userName"
+                  value={formState.userName}
+                  onChange={handleChange}
+                />
+                <TextInput
+                  label="GitHub Username*"
+                  name="githubUrl"
+                  value={formState.githubUrl}
+                  onChange={handleChange}
+                />
+              </section>
 
-      </Card>
-    </div>
-  )}
-  </>
-);
+              {profile.role === "STUDENT" ?
+                <section className="trainingInfoSection text-input-containers">
+                  <h2>Training Info</h2>
+                  <TextInput
+                    label="Role*"
+                    name="role"
+                    value={formState.role.toLocaleLowerCase()}
+                    onChange={handleChange}
+                    readOnly={readOnly}
+                  />
+                  <TextInput
+                    label="Specialism*"
+                    name="specialism"
+                    value={formState.specialism}
+                    onChange={handleChange}
+                    readOnly={readOnly}
+                  />
+                  <TextInput
+                    label="Cohort*"
+                    name="cohort_id"
+                    value={formState.cohort_id}
+                    onChange={handleChange}
+                    readOnly={readOnly}
+                  />
+                  <TextInput
+                    label="Start Date*"
+                    name="start-date"
+                    value={formState.startDate}
+                    onChange={handleChange}
+                    readOnly={readOnly}
+                  />
+                  <TextInput
+                    label="End Date*"
+                    name="end-date"
+                    value={formState.endDate}
+                    onChange={handleChange}
+                    readOnly={readOnly}
+                  />
+                </section>
+                :
+                <section className="proffesional-info-section">
+                  <h2>Proffessional Info</h2>
+                  <TextInput
+                    label="Role*"
+                    name="role"
+                    value={profile.cohort}
+                    onChange={handleChange}
+                  />
+                  <TextInput
+                    label="Specialism*"
+                    name="specialism"
+                    value={formState.specialism}
+                    onChange={handleChange}
+                  />
+                  <TextInput
+                    label="Job Title*"
+                    name="jobTitle"
+                    value={formState.specialism}
+                    onChange={handleChange}
+                  />
+                </section>
+              }
+              <section className="contactInfoSection">
+                <h2>Contact Info</h2>
+                <TextInput
+                  label="Email*"
+                  name="email"
+                  value={formState.email}
+                  type="email"
+                  onChange={handleChange}
+                />
+                <TextInput
+                  label="Mobile*"
+                  name="phone"
+                  value={formState.phone}
+                  onChange={handleChange}
+                />
+                <TextInput
+                  label="Password*"
+                  name="password"
+                  value={formState.password}
+                  type="password"
+                  onChange={handleChange}
+                  permission={passwordPermission}
+                />
+              </section>
+              <section className="bioSection">
+                <h2>Bio</h2>
+                <textarea label="Bio" name="biography" value={formState.biography} editable rows={20} maxLength="300" onChange={handleChange} />
+                <label htmlFor="bio" >{formState.biography.length}/300</label>
+              </section>
+              <section className="footer">
+                <p>Required*</p>
+                <Button text={"Cancel"} onClick={cancelChanges} classes="offwhite width-full" />
+                <Button text={"save"} type={"submit"} classes="blue width-full" />
+              </section>
+            </form>
+
+          </Card>
+        </div>
+      )}
+    </>
+  );
 };
 export default EditProfile;
 
