a5601f73529eff6eb379a0257e52dcf4471dae73
MaybeFreak <michaelpope97@gmail.com>
2023-02-09 13:10:34 +0100

    commit before revert cahnges

--- src/components/personCard/PersonCard.js
+++ src/components/personCard/PersonCard.js
@@ -1,7 +1,7 @@
 import ProfileCircle from "../../components/profileCircle";
 import './style.css'
 
-const PersonCard = ({ person }) => {
+const PersonCard = ({ person, isTeacher }) => {
 
     const name = `${person.firstName} ${person.lastName}`
     const userInitials = name.match(/\b(\w)/g)
@@ -9,22 +9,30 @@ const PersonCard = ({ person }) => {
     return (
         <li className="personCard">
             <ProfileCircle initials={userInitials}/>
+
             <div className="nameCohort">
                 <p className="personName">{name}</p>
                 {person.role === 'STUDENT' && <p className="cohortInfo">{`{Specialization}`}, Cohort {person.cohort_id}</p>}
                 {person.role === 'TEACHER' && <p className="cohortInfo">Teacher</p>}
             </div>
-            <button className="profile">
-                <p className="text-blue1">Profile</p>
-            </button>
-            <button className="addNote">
-                
-                {person.role ==='TEACHER' && <p className="text-blue1"> Add note</p>}
-            </button>
-            <button className="moveToCohort"> 
-             
-              {person.role === 'TEACHER' &&  <p className="text-blue1"> Move to cohort</p> }
-            </button>
+
+            <div className="button-wrapper">
+                <button className="profile">
+                    <p className="text-blue1">Profile</p>
+                </button>
+
+                {isTeacher && 
+                    <button className="addNote">
+                        <p className="text-blue1"> Add note</p>
+                    </button>
+                }
+
+                {isTeacher &&  
+                    <button className="moveToCohort"> 
+                        <p className="text-blue1"> Move to cohort</p>
+                    </button>
+                }
+            </div>
 
             <div className="edit-icon">
                 <p className="text-blue1">...</p>
