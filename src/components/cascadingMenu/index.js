c6c98a9583279cb9f2c930f6b3de89d8aed1ba2c
MaybeFreak <michaelpope97@gmail.com>
2023-02-09 12:49:21 +0100

    commit before merge

---
+++ src/components/cascadingMenu/index.js
@@ -0,0 +1,46 @@
+import { useRef } from "react"
+import useOnOutsideClick from "../../hooks/useOnOutsideClick"
+import AddIcon from "../../assets/icons/addIcon"
+import CohortIcon from "../../assets/icons/cohortIcon"
+import CohortIconFill from "../../assets/icons/cohortIcon-fill"
+import DeleteIcon from "../../assets/icons/deleteIcon"
+import MonitorIcon from "../../assets/icons/monitorIcon"
+import ProfileIcon from "../../assets/icons/profileIcon"
+import SquareBracketsIcon from "../../assets/icons/squareBracketsIcon"
+import MenuItem from "../menu/menuItem"
+import Menu from "../menu"
+import "./style.css"
+
+const CascadingMenu = ({ setIsMenuVisible }) => {
+    const menuRef = useRef(null)
+
+    useOnOutsideClick(menuRef, ()=>setIsMenuVisible(false))
+
+    return (
+        <div ref={menuRef}>
+            <Menu className="profile-circle-menu" >
+                <MenuItem icon={<ProfileIcon />} text='Profile' />
+                <MenuItem icon={<AddIcon />} text='Add note' />
+
+                <MenuItem icon={<CohortIcon />} text='Move to cohort'>
+                    <MenuItem icon={<SquareBracketsIcon />} text='Software Development'>
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 1' />
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 2' />
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 3' />
+                    </MenuItem>
+
+                    <MenuItem icon={<MonitorIcon />} text='Frontend Development'>
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 1' />
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 2' />
+                        <MenuItem icon={<CohortIconFill />} text='Cohort 3' />
+                    </MenuItem>
+                
+                </MenuItem>
+
+                <MenuItem icon={<DeleteIcon />} text='Delete student' />
+            </Menu>
+        </div>
+    )
+}
+
+export default CascadingMenu
