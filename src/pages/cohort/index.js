f9b867efe6d149e2794eea089d3cb40783e82e75
Rebecca Noy <rebecca.noy@gmx.de>
2024-02-06 15:26:38 +0100

    add boilerplate for Cohort.jsx

---
+++ src/pages/cohort/index.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Button from "../../components/button";
+import Card from "../../components/card";
+import useModal from "../../hooks/useModal";
+import "./style.css";
+
+export default function Cohort () {
+	return (
+		<>
+			MyCohort
+		</>
+	)
+}
