db32783265d8b27d8cf1b2df9a2d5c8aa46b7b2f
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-07 11:46:24 +0000

    feat: create new post

--- src/components/createPostModal/index.js
+++ src/components/createPostModal/index.js
@@ -1,51 +1,79 @@
-import { useState } from "react"
-import useModal from "../../hooks/useModal"
-import './style.css'
-import Button from '../button'
+import { useState } from "react";
+import useModal from "../../hooks/useModal";
+import "./style.css";
+import Button from "../button";
+import { postPost } from "../../service/apiClient";
+import jwt_decode from "jwt-decode";
 
-const CreatePostModal = () => {
-    // Use the useModal hook to get the closeModal function so we can close the modal on user interaction
-    const { closeModal } = useModal()
+const CreatePostModal = ({ getAllPosts }) => {
+  // Use the useModal hook to get the closeModal function so we can close the modal on user interaction
+  const { closeModal } = useModal();
 
-    const [message, setMessage] = useState(null)
-    const [text, setText] = useState('')
+  const [message, setMessage] = useState(null);
+  const [text, setText] = useState("");
 
-    const onChange = (e) => {
-        setText(e.target.value)
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = () => {
+    const token = localStorage.getItem("token");
+    const { userId } = jwt_decode(token);
+    const newPost = {
+      content: text,
+      userId: userId,
+    };
+
+    const createPost = (newPost) => {
+      postPost(newPost).then(getAllPosts);
+    };
+
+    try {
+      createPost(newPost);
+      setMessage("Submit button was clicked! Closing modal in 2 seconds...");
+    } catch (e) {
+      if (e.codeStatus === 400) {
+        setMessage("Oops, this post is empty!");
+      }
     }
 
-    const onSubmit = () => {
-        setMessage('Submit button was clicked! Closing modal in 2 seconds...')
+    setTimeout(() => {
+      setMessage(null);
+      closeModal();
+    }, 2000);
+  };
 
-        setTimeout(() => {
-            setMessage(null)
-            closeModal()
-        }, 2000)
-    }
+  return (
+    <>
+      <section className="create-post-user-details">
+        <div className="profile-icon">
+          <p>AJ</p>
+        </div>
+        <div className="post-user-name">
+          <p>Alex J</p>
+        </div>
+      </section>
 
-    return (
-        <>
-            <section className="create-post-user-details">
-                <div className="profile-icon"><p>AJ</p></div>
-                <div className="post-user-name"><p>Alex J</p></div>
-            </section>
+      <section>
+        <textarea
+          onChange={onChange}
+          value={text}
+          placeholder="What's on your mind?"
+        ></textarea>
+      </section>
 
-            <section>
-                <textarea onChange={onChange} value={text} placeholder="What's on your mind?"></textarea>
-            </section>
+      <section className="create-post-actions">
+        <Button
+          onClick={onSubmit}
+          text="Post"
+          classes={`${text.length ? "blue" : "offwhite"} width-full`}
+          disabled={!text.length}
+        />
+      </section>
 
-            <section className="create-post-actions">
-                <Button
-                    onClick={onSubmit}
-                    text='Post'
-                    classes={`${text.length ? 'blue' : 'offwhite' } width-full`}
-                    disabled={!text.length}
-                />
-            </section>
+      {message && <p>{message}</p>}
+    </>
+  );
+};
 
-            {message && <p>{message}</p>}
-        </>
-    )
-}
-
-export default CreatePostModal
+export default CreatePostModal;
