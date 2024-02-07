9e975036caea89033784906c620eab37fc1d3ba7
Satoki <satokiito@gmail.com>
2024-02-07 15:32:39 +0000

    updated handleCreateProfile funct with params for stepThree data

--- src/context/auth.js
+++ src/context/auth.js
@@ -48,10 +48,10 @@ const AuthProvider = ({ children }) => {
     navigate('/verification')
   }
 
-  const handleCreateProfile = async (firstName, lastName, githubUrl, bio) => {
+  const handleCreateProfile = async (firstName, lastName, githubUrl, bio, role, specialism, cohort, startDate, endDate) => {
     const { userId } = jwt_decode(token)
 
-    await createProfile(userId, firstName, lastName, githubUrl, bio)
+    await createProfile(userId, firstName, lastName, githubUrl, bio, role, specialism, cohort, startDate, endDate)
 
     localStorage.setItem('token', token)
     navigate('/')
