1b164ffd9f92ef54e275a7988a8d246856bf1d6a
Lukas Dembicki <lad_777@hotmail.com>
2024-02-08 17:39:26 +0000

    BUILD: new dropdown menu for userCard

---
+++ src/components/ProfileContextMenu/index.js
@@ -0,0 +1,19 @@
+import "./style.css";
+import ProfileIcon from "../../assets/icons/profileIcon";
+import { useNavigate } from "react-router-dom";
+
+
+
+const ProfileContextMenu = (user) => {
+  
+  const navigate = useNavigate()
+  
+    return (
+        <div onClick={navigate(`/${user.id}`)} className="profile-context-menu">
+            <ProfileIcon/>
+            <div className="profile-text">Profile</div>
+        </div>
+    );
+};
+
+export default ProfileContextMenu;
