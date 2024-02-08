46ab86055d564877c53453044e8922fc31dddedb
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-08 11:22:16 +0000

    refactor: make UsersList stateless

--- src/components/usersList/index.js
+++ src/components/usersList/index.js
@@ -1,19 +1,9 @@
 import UserCard from "../userCard";
-import { getUsers } from "../../service/apiClient";
-import { useState, useEffect } from "react";
 
-const UsersList = () => {
-    const [users, setUsers] = useState([])
-  
-    useEffect(() => {
-        getUsers()
-        .then(setUsers)
-    }, [])
-
+const UsersList = ({ users }) => {
     if (!users || users.length === 0) {
         return <></>
     }
-
     return (
         <>
            {users.map(user => {
