90012eac886893ab0dffd6fba29d345d88f7e471
Radio58 <rt58gm@gmail.com>
2024-02-08 16:26:11 +0000

    Add MyExercises Component

---
+++ src/components/myExercises/index.js
@@ -0,0 +1,26 @@
+import Card from "../card";
+import Button from "../button";
+import "./style.css"
+
+const MyExercises = () => {
+    return (
+        <>
+            <Card>
+                <h3 className="my-cohort-exercises--header">My Exercises</h3>
+                <div>
+                    <div className="my-cohort-exercises--stats">
+                        <p>Modules:</p> <span>2/7 completed</span>
+                        <p>Units:</p> <span>4/10 completed</span>
+                        <p>Exercises:</p> <span>34/58 completed</span>
+                    </div>
+                
+                    <div className="my-cohort-excercises--button">
+                        <Button text="See Exercises"/>
+                    </div>
+                </div>
+            </Card>
+        </>
+    );
+};
+
+export default MyExercises;
