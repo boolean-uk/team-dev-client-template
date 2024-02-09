e046677e946a9b16f3dcb056c320fff5d9a04134
Elizabeth <lizbeepmc@gmail.com>
2024-02-09 18:34:48 +0300

    basıcınfo

--- src/pages/welcome/stepOne/index.js
+++ src/pages/welcome/stepOne/index.js
@@ -15,25 +15,33 @@ const StepOne = ({ data, setData }) => {
             name="pictureUrl"
             label={'Photograph'}
             placeholder="please enter a valid image url"
+
+            
           />
 
           <TextInput
             onChange={setData}
             value={data.firstName}
             name="firstName"
-            label={"First name"}
+            label={"First name*"}
+            placeholder="enter your first name"
+            required
           />
           <TextInput
             onChange={setData}
             value={data.lastName}
             name="lastName"
-            label={"Last name"}
+            label={"Last name*"}
+            placeholder="enter your last name"
+            required
           />
           <TextInput
             onChange={setData}
             value={data.githubUsername}
             name="githubUsername"
-            label={"Github Username"}
+            label={"Github Username*"}
+            placeholder="enter your github user name"
+            required
           />
           <p className="text-blue1">*Required</p>
         </div>
