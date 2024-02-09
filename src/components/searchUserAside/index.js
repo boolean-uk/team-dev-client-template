b10d56240b625903835956fa8fa55f8d8b1e9483
Faiza <faraz_ul_islam@yahoo.com>
2024-02-09 12:53:56 +0000

    work needed

--- src/components/searchUserAside/index.js
+++ src/components/searchUserAside/index.js
@@ -6,6 +6,8 @@ import { getUserByName } from "../../service/apiClient";
 import Menu from "../menu";
 import UsersList from "../usersList";
 import "./style.css";
+import Button from "../button";
+
 
 import { user } from "../../service/mockData";
 
@@ -13,7 +15,7 @@ import { user } from "../../service/mockData";
 const SearchUserAside = () => {
   const [hasFocus, setHasFocus] = useState(false);
   const [searchVal, setSearchVal] = useState("");
-  const [users, setUsers] = useState([user.user]);
+  const [users, setUsers] = useState(user.user);
 
   const onChange = (e) => {
     setSearchVal(e.target.value);
@@ -29,36 +31,47 @@ const SearchUserAside = () => {
   };
 
   return (
-    <Card>
-      <form
-        onSubmit={onSubmit}
-        onFocus={() => setHasFocus(true)}
-        onBlur={() => setHasFocus(false)}
-      >
-        <TextInput
-          icon={<SearchIcon />}
-          value={searchVal}
-          name="Search"
-          onChange={onChange}
-        />
-      </form>
-      {hasFocus && <UserResults users={users}/>}
-    </Card>
+    <div className="relative">
+      <Card>
+        <form
+          onSubmit={onSubmit}
+          onFocus={() => setHasFocus(true)}
+          onBlur={() => setHasFocus(false)}
+        >
+          <TextInput
+            icon={<SearchIcon />}
+            value={searchVal}
+            name="Search"
+            onChange={onChange}
+          />
+        </form>
+      </Card>
+      <UserResults users={users} />
+
+    </div>
   );
 };
 
-const UserResults = ({users}) => {
+const UserResults = ({ users }) => {
   // TODO: need onFocus stuff for the results list
-  // TODO: results need to be outside of aside card
-  // TODO: results should overlap my cohort list
+
   // TODO: results should be same width as search bar
   // TODO: add bottom button area which changes depending on length of userListResults
 
+
+
   return (
-    <Menu className={"search-user-menu"}>
-      <h4 className="border-bottom">People</h4>
-      <UsersList users={users} />
-    </Menu>
+    <>
+      <Menu className={"search-user-menu"}>
+
+        <h4 className="border-bottom spacing">People</h4>
+        <UsersList users={users} />
+        {users.length >= 10 &&
+          <Button classes='button  offwhite  spacing' text={'All Results'} />
+        }
+      </Menu>
+
+    </>
   );
 };
 
