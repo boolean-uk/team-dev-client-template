4f954b94547d0442cfa92982e2f9c399cf4c795d
Tayokanch <tayoquadri11@gmail.com>
2024-02-13 15:45:49 +0000

    changed the style of the logo

--- src/assets/fullLogo.js
+++ src/assets/fullLogo.js
@@ -2,13 +2,14 @@ import logo from '../assets/logo.jpg'
 const FullLogo = ({ textColour = "#000046" }) => {
 
   return (
-    <figure style={{ display: "grid", gridTemplateColumns: "auto auto", alignItems: "center", justifyContent: "center", gridColumnGap: "10px" }}>
-    <img src={logo} 
-      alt='Cohort Manager Logo'
-      width='120px'
-    />
-    <h3 style={{ color: "#000046", fontSize: "32px", gridColumn: "2" }}>Cohort Manager</h3>
-  </figure>
+<figure style={{}}>
+  <img src={logo} 
+    alt='Cohort Manager Logo'
+    width='100px'
+  />
+  <h3 style={{ color: "#000046", fontSize: "32px", gridColumn: "2" }}>Cohort Manager</h3>
+</figure>
+
   
   );
 };
