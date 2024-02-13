a97bfe5d1d736e729c865c1601ab7833f8d38d05
Lukas Dembicki <lad_777@hotmail.com>
2024-02-13 17:37:30 +0000

    BUILD: I18N addition.

--- src/components/commentInput/index.js
+++ src/components/commentInput/index.js
@@ -3,8 +3,11 @@ import ProfileCircle from "../profileCircle"
 import "./style.css"
 import sendIcon from "../../assets/icons/send.png"
 import { postComment } from "../../service/apiClient"
+import { useTranslation } from "react-i18next"
 
 const CommentInput = ({ postId, getAllPosts }) => {
+
+  const {t} = useTranslation()
   const [content, setContent] = useState("")
 
   const submitHandler = (e) => {
@@ -23,14 +26,14 @@ const CommentInput = ({ postId, getAllPosts }) => {
       <form onSubmit={submitHandler} className="commentInput__content">
         <input
           type="text"
-          placeholder="Add a comment..."
+          placeholder={t("addAComment")}
           className="commentInput__content-input"
           value={content}
           onChange={changeHandler}
           required
         />
         <button className="commentInput__content-button">
-          <img src={sendIcon} alt="send message" />
+          <img src={sendIcon} alt={t("sendMessage")} />
         </button>
       </form>
     </section>
