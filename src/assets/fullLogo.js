c381a272ae08facb03fecef11c20087c3378ff31
Tayokanch <tayoquadri11@gmail.com>
2024-02-13 13:55:57 +0000

    added a column gap to the figure tag

--- src/assets/fullLogo.js
+++ src/assets/fullLogo.js
@@ -2,14 +2,14 @@ import logo from '../assets/logo.jpg'
 const FullLogo = ({ textColour = "#000046" }) => {
 
   return (
-    <figure>
-      <img src={logo} 
+    <figure style={{ display: "grid", gridTemplateColumns: "auto auto", alignItems: "center", justifyContent: "center", gridColumnGap: "10px" }}>
+    <img src={logo} 
       alt='Cohort Manager Logo'
       width='120px'
- 
-      />
-      <h3 style={{color: "#000046",  fontSize: "48px"}}>Cohort Manager</h3>
-    </figure>
+    />
+    <h3 style={{ color: "#000046", fontSize: "32px", gridColumn: "2" }}>Cohort Manager</h3>
+  </figure>
+  
   );
 };
 
