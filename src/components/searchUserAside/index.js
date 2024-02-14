14036d1a0fb498ba67a825145846bef172c3e17a
Pierluigi <pierluigi.capirci89@gmail.com>
2024-02-14 12:07:13 +0000

    update format document

--- src/components/searchUserAside/index.js
+++ src/components/searchUserAside/index.js
@@ -17,32 +17,32 @@ const SearchUserAside = () => {
   const [menuHover, setMenuHover] = useState(false)
   const [searchVal, setSearchVal] = useState("")
   const [users, setUsers] = useState([])
-  
+
   const menuOpen = hasFocus || searchVal.length > 0 || menuHover
-  
+
   const getUsers = () => {
     getUserByName(searchVal).then(setUsers)
   }
-  
+
   useEffect(getUsers, [searchVal])
-  
+
   return (
     <div className="relative">
       <Card>
         <form
           onFocus={() => setHasFocus(true)}
           onBlur={() => setHasFocus(false)}
-          >
+        >
           <UserSearchBar searchVal={searchVal} setSearchVal={setSearchVal} />
         </form>
       </Card>
       {menuOpen && (
         <UserResults
-        users={users}
-        menuHover={menuHover}
-        setMenuHover={setMenuHover}
+          users={users}
+          menuHover={menuHover}
+          setMenuHover={setMenuHover}
         />
-        )}
+      )}
     </div>
   )
 }
@@ -52,19 +52,22 @@ const UserResults = ({ users, setMenuHover }) => {
   if (!Array.isArray(users)) {
     users = []
   }
-  
+
   return (
     <>
       <div
         onMouseEnter={() => setMenuHover(true)}
         onMouseLeave={() => setMenuHover(false)}
-        >
+      >
         <Menu className={"search-user-menu"}>
           <p className="border-bottom spacing text-blue1">{t("people")}</p>
           <UsersList users={users.slice(0, 10)} />
           {users.length >= 10 && (
             <NavLink to="/results">
-              <Button classes="button offwhite spacing" text={t("allResults")} />
+              <Button
+                classes="button offwhite spacing"
+                text={t("allResults")}
+              />
             </NavLink>
           )}
           {users.length === 0 && <NoResults />}
@@ -83,8 +86,8 @@ const NoResults = () => {
           {t("noResultsFound")}
           <br />
           <br />
-          
-        {t("changeSearch")}
+
+          {t("changeSearch")}
         </p>
       </div>
       <NavLink to="/results">
