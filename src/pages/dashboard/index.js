802e683c0ef667814c197485833a2ba3a074dfe0
Chloe Zermatten <chloezermatten@gmail.com>
2024-02-15 14:18:43 +0000

    feat: dashboard only shows a Cohorts section if a teacher is viewing the page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -5,13 +5,15 @@ import CreatePostModal from "../../components/createPostModal"
 import Posts from "../../components/posts"
 import useModal from "../../hooks/useModal"
 import "./style.css"
-import { getPosts, getUsers } from "../../service/apiClient"
+import { getCohorts, getPosts, getUsers } from "../../service/apiClient"
 import UsersList from "../../components/usersList"
 import SearchUserAside from "../../components/searchUserAside"
+import CohortList from "../../components/cohortList"
 
 const Dashboard = () => {
   const [posts, setPosts] = useState([])
   const [users, setUsers] = useState([])
+  const [cohorts, setCohorts] = useState(null)
 
   const sortPosts = (fetchedPosts) => {
     const sortedPosts = fetchedPosts.sort(
@@ -32,17 +34,34 @@ const Dashboard = () => {
   const getAllUsers = () => {
     getUsers().then(setUsers)
   }
-
+  
+  const getAllCohorts = () => {
+    getCohorts().then(setCohorts)
+  }
+  
   useEffect(getAllPosts, [])
   useEffect(getAllUsers, [])
-
-
+  useEffect(getAllCohorts, [])
+  
   const { openModal, setModal } = useModal()
-
+  
   const showModal = () => {
     setModal("Create a post", <CreatePostModal getAllPosts={getAllPosts} />)
-
+    
     openModal()
+  }
+  
+  const shouldRenderCohortList = () => !!Array.isArray(cohorts)
+
+  const showCohorts = () => {
+    if (!shouldRenderCohortList()) {
+      return <></>
+    }
+    return (
+      <Card header={"Cohorts"}>
+        <CohortList cohorts={cohorts} />
+      </Card>
+    )
   }
 
   return (
@@ -61,10 +80,10 @@ const Dashboard = () => {
       </main>
       <aside>
         <SearchUserAside />
-        <Card>
-          <h4>My Cohort</h4>
+        <Card header={"My Cohort"}>
           <UsersList users={users} />
         </Card>
+        {showCohorts()}
       </aside>
     </>
   )
