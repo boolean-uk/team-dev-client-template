0e9fe9cbe1f7e11b34d3dc7999cb46be81cb98cd
Nazar Tymiv <nazartumiv@gmail.com>
2024-02-15 10:40:31 +0000

    Remove timeout for create post modal

--- src/components/createPostModal/index.js
+++ src/components/createPostModal/index.js
@@ -1,47 +1,43 @@
-import { useState } from "react";
-import useModal from "../../hooks/useModal";
-import "./style.css";
-import Button from "../button";
-import { postPost } from "../../service/apiClient";
-import jwtDecode from "jwt-decode";
+import { useState } from "react"
+import useModal from "../../hooks/useModal"
+import "./style.css"
+import Button from "../button"
+import { postPost } from "../../service/apiClient"
+import jwtDecode from "jwt-decode"
 
 const CreatePostModal = ({ getAllPosts }) => {
   // Use the useModal hook to get the closeModal function so we can close the modal on user interaction
-  const { closeModal } = useModal();
+  const { closeModal } = useModal()
 
-  const [message, setMessage] = useState(null);
-  const [text, setText] = useState("");
+  const [message, setMessage] = useState(null)
+  const [text, setText] = useState("")
 
   const onChange = (e) => {
-    setText(e.target.value);
-  };
+    setText(e.target.value)
+  }
 
   const onSubmit = () => {
-    const token = localStorage.getItem("token");
-    const { userId } = jwtDecode(token);
+    const token = localStorage.getItem("token")
+    const { userId } = jwtDecode(token)
     const newPost = {
       content: text,
       userId,
-    };
-
-    const createPost = (newPost) => {
-      postPost(newPost).then(getAllPosts);
-    };
-
-    try {
-      createPost(newPost);
-      setMessage("Submit button was clicked! Closing modal in 2 seconds...");
-    } catch (e) {
-      if (e.codeStatus === 400) {
-        setMessage("Oops, this post is empty!");
-      }
     }
 
-    setTimeout(() => {
-      setMessage(null);
-      closeModal();
-    }, 2000);
-  };
+    const createPost = (newPost) => {
+      postPost(newPost).then(getAllPosts)
+    }
+
+    try {
+      createPost(newPost)
+      setMessage(null)
+      closeModal()
+    } catch (e) {
+      if (e.codeStatus === 400) {
+        setMessage("Oops, this post is empty!")
+      }
+    }
+  }
 
   return (
     <>
@@ -71,7 +67,7 @@ const CreatePostModal = ({ getAllPosts }) => {
 
       {message && <p>{message}</p>}
     </>
-  );
-};
+  )
+}
 
-export default CreatePostModal;
+export default CreatePostModal
