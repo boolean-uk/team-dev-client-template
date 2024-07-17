89585e7114d2d4fd83740dc21535981406504b42
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-07-17 11:36:03 +0200

    change step 2 to step 4

--- src/pages/welcome/stepTwo/index.js
+++ src/pages/welcome/stepTwo/index.js
@@ -3,15 +3,7 @@ import Form from "../../../components/form"
 const StepTwo = ({ data, setData }) => {
     return (
         <>
-            <div className="welcome-formheader">
-                <h3>Bio</h3>
-            </div>
-            <Form className="welcome-form">
-                <div className="welcome-form-inputs">
-                    <textarea name="bio" value={data.bio} onChange={setData}></textarea>
-                    <p className="text-blue1">*Required</p>
-                </div>
-            </Form>
+
         </>
     )
 }
