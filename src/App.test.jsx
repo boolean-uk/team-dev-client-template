c0963c2ee143819c793e3541e827ce70ac3fc370
Tim Zoltie <t.zoltie@gmail.com>
2024-07-24 17:01:02 +0100

    set up for fronten d tests, dependencies installed etc

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom'
+// import App from './App';
+import Login from './pages/login';
+import Button from './components/button';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+test('it should not send an empty login form ', () => {
+  const handleSubmit = jest.fn()
+  render(<Login />)
+  render(<Button onClick={handleSubmit} />)
+  const submitButton = screen.getByRole('button', { text: /log in/i})
+})
