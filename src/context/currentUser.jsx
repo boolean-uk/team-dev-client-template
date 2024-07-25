b9052fec4b262e30f19552ac044524d29096263c
Angus Townsley <angustownsley@gmail.com>
2024-07-25 10:24:52 +0100

    Redirect users to profile stepper if firstName is missing

--- src/context/currentUser.jsx
+++ src/context/currentUser.jsx
@@ -2,16 +2,19 @@ import { createContext, useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
 import jwt_decode from 'jwt-decode'
 import { getUser } from '../service/apiClient'
+import { useLocation, useNavigate, redirect } from 'react-router-dom'
 
 export const CurrentUserContext = createContext()
 
 export const CurrentUserProvider = ({ children }) => {
     const { token, user } = useAuth()
     const [currentUser, setCurrentUser] = useState(null)
+    const location = useLocation()
+    const navigate = useNavigate()
 
     useEffect(() => {
         if (user) {
-            setCurrentUser({ ...user })
+            setCurrentUser({ ...user.user })
             return
         }
 
@@ -21,18 +24,32 @@ export const CurrentUserProvider = ({ children }) => {
                 const userDetails = await getUser(userId)
 
                 if (userDetails.status === 'success') {
-                    setCurrentUser({ ...userDetails.data.user })
+                    setCurrentUser({ ...userDetails.data.user.user })
                     return
                 }
             }
             setCurrentUser(null)
             return
         }
-        if (!currentUser) {
+        if (token) {
             getUserFromToken()
         }
-        return
-    }, [token, user])
+    }, [token, user, location.pathname])
+
+    useEffect(() => {
+        if (
+            token &&
+            currentUser &&
+            !currentUser?.firstName &&
+                (location.pathname !== '/welcome' || location.pathname !== 'verification')
+        ) {
+            navigate('/welcome')
+        }
+
+        if (location.pathname === '/welcome' && currentUser?.firstName) {
+            navigate('/')
+        }
+    }, [token, currentUser, location.pathname])
 
     return (
         <CurrentUserContext.Provider
