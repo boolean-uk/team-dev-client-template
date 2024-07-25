e2c4f40b9d250a716b3a1636376aa28fa28e5a3e
Will Baxter <bax.will@googlemail.com>
2024-07-25 11:14:50 +0100

    Added route and nav bar link:

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,51 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/dashboard'
-import Login from './pages/login'
-import Register from './pages/register'
-import Loading from './pages/loading'
-import Verification from './pages/verification'
-import { AuthProvider, ProtectedRoute } from './context/auth'
-import { ModalProvider } from './context/modal'
-import { CurrentUserProvider } from './context/currentUser'
-import Welcome from './pages/welcome'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Loading from "./pages/loading";
+import Verification from "./pages/verification";
+import Profile from "./pages/profile";
+import { AuthProvider, ProtectedRoute } from "./context/auth";
+import { ModalProvider } from "./context/modal";
+import { CurrentUserProvider } from "./context/currentUser";
+import Welcome from "./pages/welcome";
 
 const App = () => {
-    return (
-        <>
-            <AuthProvider>
-                <CurrentUserProvider>
-                    <ModalProvider>
-                        <Routes>
-                            <Route path="login" element={<Login />} />
-                            <Route path="register" element={<Register />} />
-                            <Route path="loading" element={<Loading />} />
-                            <Route
-                                path="verification"
-                                element={<Verification />}
-                            />
+  return (
+    <>
+      <AuthProvider>
+        <CurrentUserProvider>
+          <ModalProvider>
+            <Routes>
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="loading" element={<Loading />} />
+              <Route path="verification" element={<Verification />} />
+              <Route path="profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+             
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="welcome"
+                element={
+                  <ProtectedRoute disabledNav={true}>
+                    <Welcome />
+                  </ProtectedRoute>
+                }
+              />
+            </Routes>
+          </ModalProvider>
+        </CurrentUserProvider>
+      </AuthProvider>
+    </>
+  );
+};
 
-                            <Route
-                                index
-                                element={
-                                    <ProtectedRoute>
-                                        <Dashboard />
-                                    </ProtectedRoute>
-                                }
-                            />
-                            <Route
-                                path="welcome"
-                                element={
-                                    <ProtectedRoute disabledNav={true}>
-                                        <Welcome />
-                                    </ProtectedRoute>
-                                }
-                            />
-                        </Routes>
-                    </ModalProvider>
-                </CurrentUserProvider>
-            </AuthProvider>
-        </>
-    )
-}
-
-export default App
+export default App;
---
+++ src/pages/profile/index.jsx
@@ -0,0 +1,12 @@
+import Header from "../../components/header";
+import Navigation from "../../components/navigation";
+
+const Profile = () => {
+  return (
+    <>
+<p>Profile</p>
+    </>
+  );
+};
+
+export default Profile;
