9efa52df642c804e2b61471950ac07545cd104bf
Periklis <perik76@gmail.com>
2024-07-26 16:57:57 +0300

    refactored navigation to check for role and render icons accordingly

--- src/components/navigation/index.jsx
+++ src/components/navigation/index.jsx
@@ -2,65 +2,80 @@ import { NavLink, useLocation } from 'react-router-dom'
 import CohortIcon from '../../assets/icons/cohortIcon'
 import HomeIcon from '../../assets/icons/homeIcon'
 import ProfileIcon from '../../assets/icons/profileIcon'
+import ExercisesIcon from '../../assets/icons/exercisesIcon'
+import LogsIcon from '../../assets/icons/logsIcon'
+import NotesIcon from '../../assets/icons/notesIcon'
 import useAuth from '../../hooks/useAuth'
+import useUser from '../../hooks/useUser'
 import './style.css'
 
 const Navigation = ({ disabledNav = false }) => {
     const { token } = useAuth()
+    const { currentUser } = useUser()    
     const location = useLocation()
+    console.log(currentUser);
 
     if (!token) {
         return null
     }
+    if (!currentUser) {
+        return null
+    }
 
-    if (disabledNav) {
-        return (
-            <nav>
-            <ul>
-                <li>
-                    <NavLink to={location.pathname} end>
-                        <HomeIcon />
-                        <p>Home</p>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={location.pathname} end>
-                        <ProfileIcon />
-                        <p>Profile</p>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to={location.pathname} end>
-                        <CohortIcon />
-                        <p>Cohort</p>
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-        )
+
+    const navIcons = () => {
+        const commonIcons = [
+            {
+                to: "/",
+                icon: <HomeIcon colour="#000046" />,
+                text: "Home"
+            },
+            {
+                to: "/",
+                icon: <ProfileIcon />,
+                text: "Profile"
+            },
+            {
+                to: "/",
+                icon: <CohortIcon />,
+                text: "Cohort"
+            },
+            {
+                to: '/',
+                icon: <ExercisesIcon />,
+                text: 'Exercises'
+            }
+        ]
+
+        const teacherIcons = [
+            ...commonIcons,
+            {
+                to: "/",
+                icon: <NotesIcon />,
+                text: "Notes"
+            },
+            {
+                to: "/",
+                icon: <LogsIcon />,
+                text: "Logs"
+            }
+        ]
+        const iconsToShow = currentUser.role === 'TEACHER' ? teacherIcons : commonIcons
+
+        return iconsToShow.map((icon, index) => (
+            <li key={index}>
+                <NavLink to={disabledNav ? location.pathname : icon.to} end={disabledNav}>
+                    {icon.icon}
+                    <p>{icon.text}</p>
+                </NavLink>
+            </li>
+        ))
     }
 
     return (
         <nav>
             <ul>
-                <li>
-                    <NavLink to="/">
-                        <HomeIcon colour="#000046" />
-                        <p>Home</p>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">
-                        <ProfileIcon />
-                        <p>Profile</p>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/">
-                        <CohortIcon />
-                        <p>Cohort</p>
-                    </NavLink>
-                </li>
+                {navIcons()}
             </ul>
         </nav>
     )
