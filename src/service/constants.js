ac1c0fcb44d892d02e6b8147bd265eb40758abcb
Dag Andreas <daanfofo@gmail.com>
2024-10-29 11:53:14 +0100

    now using env for api_url

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,3 +1,2 @@
-export const API_URL = 'http://localhost:4000';
-
-// export const API_URL = process.env.REACT_APP_API_URL;
+export const API_URL = process.env.I_URL;
+// export const API_URL = 'http://localhost:4000';
