f51249f74d5aec4384a1b63a635c7a0d76d6ab49
Valen98 <leo.wahlandtcs@gmail.com>
2024-10-30 15:35:38 +0100

    Added so you can see all the students and teacher of current cohort

--- src/service/apiClient.js
+++ src/service/apiClient.js
@@ -18,6 +18,11 @@ async function getPosts() {
   return res.data.posts;
 }
 
+async function getCohorts() {
+  const res = await get('cohorts');
+  return res.data.cohorts;
+}
+
 async function post(endpoint, data, auth = true) {
   return await request('POST', endpoint, data, auth);
 }
@@ -52,4 +57,4 @@ async function request(method, endpoint, data, auth = true) {
   return response.json();
 }
 
-export { login, getPosts, register, createProfile };
+export { login, getPosts, register, createProfile, getCohorts };
