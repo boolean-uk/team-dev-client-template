3286d10099d36b6064430c66b754e4c04f15c44a
Simon Fredriksson <128689713+SimpFred@users.noreply.github.com>
2024-10-30 16:53:36 +0100

    Refactor ContactInfoForm to validate input fields with specific characters

--- src/pages/profilePage/components/ContactInfoForm.jsx
+++ src/pages/profilePage/components/ContactInfoForm.jsx
@@ -18,6 +18,7 @@ const ContactInfoForm = () => {
             value={profile.email}
             onChange={handleInputChange}
             type="email"
+            validChars="A-Za-z0-9@.-_"
             isRequired={true}
           />
           <TextInput
