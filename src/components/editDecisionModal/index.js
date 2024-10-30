af5bf38c2e4a58642c977f517e5b2173736779bf
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-30 10:40:43 +0100

    added comment for using wrong icon.

--- src/components/editDecisionModal/index.js
+++ src/components/editDecisionModal/index.js
@@ -6,10 +6,11 @@ const EditDecisionModal = ({ onClick }) => {
   return (
     <section className="edit-decision-container">
       <div className="decision-icon-container" onClick={() => onClick('Edit post')}>
+        {/* Using add icon since I cant find the edit icon in assets/icons. */}
         <AddIcon />
         <p>Edit post</p>
       </div>
-      <div className="decision-icon-container" onClick={() => onClick('Delete post')}>
+      <div className="decision-icon-container" onClick={() => onClick('Delete post?')}>
         <DeleteIcon />
         <p>Delete post</p>
       </div>
