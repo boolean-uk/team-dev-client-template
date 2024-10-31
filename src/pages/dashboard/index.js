a27b9783bbbd5eda29b64d44bc1e682270ca49d9
Valen98 <leo.wahlandtcs@gmail.com>
2024-10-31 11:36:59 +0100

    Finished with the cohort list on the dashboard and so when the user clicks on one of them, the user will be directed to the specific cohort

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import SearchIcon from '../../assets/icons/searchIcon';
 import Button from '../../components/button';
 import Card from '../../components/card';
@@ -7,9 +7,34 @@ import TextInput from '../../components/form/textInput';
 import Posts from '../../components/posts';
 import useModal from '../../hooks/useModal';
 import './style.css';
+import { getCohorts } from '../../service/apiClient';
+import useAuth from '../../hooks/useAuth';
+
+// eslint-disable-next-line camelcase
+import jwt_decode from 'jwt-decode';
+import { NavLink } from 'react-router-dom';
 
 const Dashboard = () => {
   const [searchVal, setSearchVal] = useState('');
+  const [cohorts, setCohorts] = useState([]);
+  const { token } = useAuth();
+
+  useEffect(() => {
+    const fetchCohorts = async () => {
+      const allCohorts = await getCohorts();
+      const cohortToAdd = [];
+      const { userId } = jwt_decode(token);
+      // Map out the cohorts to check if the user is in the cohort
+      allCohorts.forEach((cohort) => {
+        if (cohort.users.some((user) => user.id === userId)) {
+          cohortToAdd.push(cohort);
+        }
+      });
+      // SET STARTING POINT OF THE COHORT TO THE FIRST COHORT IN THE ARRAY
+      setCohorts(cohortToAdd);
+    };
+    fetchCohorts();
+  }, []);
 
   const onChange = (e) => {
     setSearchVal(e.target.value);
@@ -50,7 +75,32 @@ const Dashboard = () => {
         </Card>
 
         <Card>
-          <h4>My Cohort</h4>
+          <div className="cohort-title dashboard-cohort-title">
+            <h4>Cohorts</h4>
+          </div>
+          <div className="dashboard-cohort">
+            {cohorts.map((cohort) => {
+              return (
+                <NavLink
+                  to="/cohort"
+                  state={{ cohortId: cohort.id }}
+                  key={cohort.id}
+                  className={'cohort-nav-link'}
+                >
+                  <div className="post-details cohort-div">
+                    <div className="profile-icon">
+                      <p>&lt; &gt;</p>
+                    </div>
+                    <div className="post-user-name cohort-student-name">
+                      <p>
+                        {cohort.name} {cohort.id}
+                      </p>
+                    </div>
+                  </div>
+                </NavLink>
+              );
+            }, [])}
+          </div>
         </Card>
       </aside>
     </>
