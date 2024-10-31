3f1721e71bef470827d5c93ec22cb53640b8825a
Stian Rusvik <stian.rusvik@no.experis.com>
2024-10-31 10:16:00 +0100

    implemented delete post api function in DeletePostModal.

--- src/components/deletePostModal/index.js
+++ src/components/deletePostModal/index.js
@@ -1,9 +1,39 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { deletePost } from '../../service/apiClient';
+import useModal from '../../hooks/useModal';
 import Button from '../button';
 import './style.css';
 
-const DeletePostModal = () => {
+const DeletePostModal = ({ postId }) => {
+  const { closeModal } = useModal();
+  const navigate = useNavigate();
   const [activeBtn, setActiveBtn] = useState('Cancel');
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState(null);
+
+  console.log(`Post ID: ${postId}`);
+
+  const handleDelete = async () => {
+    try {
+      setIsLoading(true);
+      const response = await deletePost(`posts/${postId}`);
+
+      if (response.status === 'fail') {
+        throw new Error(response.message || 'Failed to delete post');
+      }
+
+      setMessage('Post deleted successfully!');
+      setTimeout(() => {
+        closeModal();
+        navigate(0);
+      }, 2000);
+    } catch (error) {
+      setMessage('Error deleting post: ' + error.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <section className="delete-post-container">
@@ -12,14 +42,20 @@ const DeletePostModal = () => {
         <Button
           text={'Cancel'}
           classes={`btn ${activeBtn === 'Cancel' ? 'active-btn' : ''}`}
-          onClick={() => setActiveBtn('Cancel')}
+          onClick={closeModal}
+          disabled={isLoading}
         />
         <Button
           text={'Delete post'}
           classes={`btn ${activeBtn === 'Delete' ? 'active-btn' : ''}`}
-          onClick={() => setActiveBtn('Delete')}
+          onClick={() => {
+            setActiveBtn('Delete');
+            handleDelete();
+          }}
+          disabled={isLoading}
         />
       </div>
+      {message && <p>{message}</p>}
     </section>
   );
 };
