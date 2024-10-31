f941bde5e2e474b91be70d383a95d64ca3d1ff22
agrmet <agron-metaj@hotmail.com>
2024-10-31 11:45:46 +0100

    Added arrowleftIcon

---
+++ src/assets/icons/arrowLeftIcon.js
@@ -0,0 +1,11 @@
+const arrowLeftIcon = () => (
+  <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="24" cy="24" r="20" fill="#F0F5FA" />
+    <path
+      d="M24 36L12 24L24 12L25.575 13.575L16.275 22.875H36V25.125H16.275L25.575 34.425L24 36Z"
+      fill="#64648C"
+    />
+  </svg>
+);
+
+export default arrowLeftIcon;
