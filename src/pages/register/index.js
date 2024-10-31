03d1679fdc238bbf5f83dfd7599bd06534419497
shyye <emma.art.dev@gmail.com>
2024-10-31 09:06:45 +0100

    Add '*Required' text

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -69,6 +69,7 @@ const Register = () => {
             />
           </form>
           {errorMessage && <ErrorMessage message={errorMessage} />}
+          <div className={`text-register-info`}>*Required</div>
           <Button
             text="Sign up"
             onClick={() => onRegister(formData.email, formData.password, setErrorMessage)}
